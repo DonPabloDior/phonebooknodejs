c486fff21757b34b40c200f821cec694119bd70a
Yermukhammet Makhanbetov <105530483+DonPabloDior@users.noreply.github.com>
2023-12-21 10:50:28 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-let xlsx =require("xlsx")
+const xlsx = require('xlsx');
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const cors = require('cors');
+
 app.use(cors());
-app.get('/jsonData', (req, res) => {
-    let wb = xlsx.readFile('БазаОСАстана.xlsx');
-    let ws = wb.Sheets['База ОС'];
-    let data = xlsx.utils.sheet_to_json(ws);
-  
+
+app.get('/jsonData', async (req, res) => {
+  try {
+    // Assuming the Excel file is hosted on a server, replace the URL with the actual URL
+    const excelFileUrl = 'https://doc.prots.kz/sip/viamedis/phonebook/БазаОСАстана.xlsx';
+
+    // Fetch the Excel file using axios
+    const response = await axios.get(excelFileUrl, { responseType: 'arraybuffer' });
+
+    // Parse the Excel file content
+    const wb = xlsx.read(response.data, { type: 'buffer' });
+    const ws = wb.Sheets['База ОС'];
+    const data = xlsx.utils.sheet_to_json(ws);
+
     let filteredData = data.filter(item => item['Тип'] === 'Внутренний номер');
     let filteredMore = filteredData.filter(item => item['Фирма'] === 'VIAMEDIS');
-  
+
     // Create an array to store the filtered results
     let resultsArray = [];
-    let result = {}
+    let result = {};
     filteredMore.forEach(item => {
       let result = {
-        'Отделение': item['Отделение'],
         'Город': item['Город'],
         'Фирма': item['Фирма'],
         'Наименование': item['Наименование'],
@@ -26,15 +37,18 @@ app.get('/jsonData', (req, res) => {
       };
       resultsArray.push(result);
     });
-  
+
     // Convert the array to JSON
     const jsonData = JSON.stringify(resultsArray, null, 2);
-  
+
     // Send the JSON data as the API response
     res.json({ resultsArray });
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+  } catch (error) {
+    console.error('Error fetching or processing the Excel file:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
