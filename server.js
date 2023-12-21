bf3b51a90c99ab4693060b71ca47fb64fcaeb1ae
Yermukhammet Makhanbetov <105530483+DonPabloDior@users.noreply.github.com>
2023-12-21 09:32:03 +0600

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let xlsx =require("xlsx")
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(cors());
 app.get('/jsonData', (req, res) => {
